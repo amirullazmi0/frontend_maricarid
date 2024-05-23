d1e553aab71cb34f91f4953f54cf95d3a398e51f
amirullazmi0 <amirullazmi0@gmail.com>
2024-05-24 00:13:50 +0700

    update

--- app/admin/client/Section.tsx
+++ app/admin/client/Section.tsx
@@ -14,6 +14,7 @@ const Section = () => {
             <div className="flex justify-center p-4">
                 <div className="lg:w-[70%] w-[100%] p-4 bg-black">
                     <Alert />
+                    <div className="pb-1 pr-5 border-b-4 border-white w-fit text-2xl font-bold uppercase text-white mb-2">Client Maricar</div>
                     <button onClick={() => handleNavigation('add')} className='btn btn-warning rounded-sm'>Add Client</button>
                     <TableClient />
                 </div>
